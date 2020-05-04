a5dbcb7cbaab3876056a44c547398b6c57ed9ab7
Malcom <mjonesjr90@gmail.com>
2020-05-04 15:59:44 -0400

    Initial Commit

---
+++ Graph Tester/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Graph Tester
+//
+//  Created by Malcom Jones on 5/4/20.
+//  Copyright © 2020 Malcom Jones. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
